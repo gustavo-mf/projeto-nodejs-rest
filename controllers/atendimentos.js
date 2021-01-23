c9966dee4649a8022edcfcb3bd2fa58bb8037ba0
Gustavo-mf <gustavofreitas@frigelar.com.br>
2021-01-23 18:24:36 -0300

    Enviando arquivos e finalizando rotas basicas

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,11 +1,31 @@
+const Atendimento = require('../models/atendimentos');
 
 module.exports = app => {
   app.get('/atendimentos', (req, res) => {
-    res.send('Voce esta na rota de atendimento e esta realizando get.');
+    Atendimento.lista(res);
+  });
+
+  app.get('/atendimentos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    Atendimento.buscaPorId(id, res);
   });
 
   app.post('/atendimentos', (req, res) => {
-    console.log(req.body);
-    res.send('Voce esta na rota de atendimentos e esta realizando um post.');
+    const atendimento = req.body;
+
+    Atendimento.adiciona(atendimento, res);
+  });
+
+  app.patch('/atendimentos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const valores = req.body;
+
+    Atendimento.altera(id, valores, res);
+  });
+
+  app.delete('/atendimentos/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+
+    Atendimento.deleta(id, res);
   });
 };
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const custemExpress = require('./config/customExpress');
+const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
-const app = custemExpress();
+conexao.connect((erro) => {
+  if(erro) {
+    console.log(erro);
+  } else {
+    console.log('conectou com sucesso');
+    Tabelas.init(conexao);
+    const app = custemExpress();
 
-app.listen(3000, () => {
-  console.log('Servidor rodando na porta 3000');
-})
-
+    app.listen(3000, () => {
+      console.log('Servidor rodando na porta 3000');
+    })
+  }
+});
